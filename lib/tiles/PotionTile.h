9ce9842c8a5b6b673c127a104379defa8bc4527a
stanleylew5 <stanley43595@gmail.com>
2024-11-18 01:18:17 -0800

    slight capitalization changes

--- lib/tiles/PotionTile.h
+++ lib/tiles/PotionTile.h
@@ -11,9 +11,9 @@ class PotionTile : public Tile{
         string potion;
         vector<string> dialogue = {
             "Ah, an__! Just what I needed—a new addition to my collection of random things that will definitely be useful… eventually… hopefully! \n \n",
-            "Look at that! A shiny new __! I’ll take it—who knows when I’ll need to bribe a cave troll? \n \n",
-            "Ah, a useful Potion! Just what I need to convince myself I’m still making progress in this nightmare. \n \n",
-            "You wandered into an Potion! You reach down to pick up this shiny new object… \n \n",
+            "Look at that! A shiny new " + potion + "! I’ll take it—who knows when I’ll need to bribe a cave troll? \n \n",
+            "Ah, a useful potion! Just what I need to convince myself I’m still making progress in this nightmare. \n \n",
+            "You wandered into an potion! You reach down to pick up this shiny new object… \n \n",
             "You’ve received a " + potion + "! You wonder what you’ll do with it… \n \n"
         };
 };

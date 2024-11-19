08e9e87e9d9c21ee13a477e48e39efe9387ebfb7
stanleylew5 <stanley43595@gmail.com>
2024-11-18 17:35:26 -0800

    Add unit tests for player movement

--- test/testMovement.cpp
+++ test/testMovement.cpp
@@ -20,3 +20,23 @@ TEST(MapMovement, MapMovementUpRightRight) {
     EXPECT_EQ(player.getPositionX(), 2); // Check X-coordinate
     EXPECT_EQ(player.getPositionY(), 6); // Check Y-coordinate
 }
+
+TEST(MapMovement, MapMovementRightRightRightUpUpUpLeftDown) {
+    Player player;
+    Map map;
+
+    istringstream input("d\nd\nd\nw\nw\nw\na\ns\n");
+    cin.rdbuf(input.rdbuf());
+
+    player.move(map); // Reads 'd'
+    player.move(map); // Reads 'd'
+    player.move(map); // Reads 'd'
+    player.move(map); // Reads 'w'
+    player.move(map); // Reads 'w'
+    player.move(map); // Reads 'w'
+    player.move(map); // Reads 'a'
+    player.move(map); // Reads 's'
+
+    EXPECT_EQ(player.getPositionX(), 2); // Check X-coordinate
+    EXPECT_EQ(player.getPositionY(), 5); // Check Y-coordinate
+}

90f99517bbd7db7d738eb393778bc73183f36a4d
Sdrabla <sdrab001@ucr.edu>
2024-11-21 13:45:09 -0800

    combined screen class header files and tested default map print. Also update dgit ignore

--- test/testScreens.cpp
+++ test/testScreens.cpp
@@ -1,38 +1,61 @@
-#include "Screen.hpp"
+#include "../lib/Screen.h"
+#include "../lib/Map.h"
+#include "../lib/Player.h"
+#include "../lib/tiles/Tile.h"
 #include <iostream>
 using namespace std;
 
-//used g++ -I../lib/screens testScreens.cpp ../src/screens/Screen.cpp -o screenTest to compile 
+//used g++ -I.testScreens.cpp ../src/Screen.cpp -o screenTest to compile 
 int main()
 {
     Screen screenTest;
 
     //test start screen 
-    cout << "Start screen:" << endl;
-    screenTest.displayStartScreen();
+    // cout << "Start screen:" << endl;
+    // screenTest.displayStartScreen();
 
-    cout << endl << endl;
+    // cout << endl << endl;
 
-    //test instruciton screen 
-    cout << "Instructions: " << endl;
-    screenTest.displayInstructions();
+    // // //test instruciton screen 
+    // cout << "Instructions: " << endl;
+    // screenTest.displayInstructions();
 
-    //test intro display
-    cout << "Intro:" << endl;
-    screenTest.displayIntroScreen();
+    // //test intro display
+    // cout << "Intro:" << endl;
+    // screenTest.displayIntroScreen();
 
-    //test command manu 
-    cout << "Command screen:" << endl;
-    screenTest.displayCommandMenu();
+    // test command manu 
+    // cout << "Command screen:" << endl;
+    // screenTest.displayCommandMenu();
 
-    //test inventory print 
-    cout << "Inventory screen:" << endl;
-    screenTest.displayInventory();
+    // //test inventory print 
+    // //Needs items and weapons 
+    // cout << "Inventory screen:" << endl;
+    // screenTest.displayInventory();
+
+    // //test credit print
+    // cout << "Credits:" << endl;
+    // screenTest.displayCredits();
+
+    //test map display
+    cout << "Map Display:" << endl;
+    
+    //create objects
+    Map map;
+    map.initialize();
+
+    Player player;
 
 
-    //test credit print
-    cout << "Credits:" << endl;
-    screenTest.displayCredits();
+    //mark first tile as visited
+    Tile tile;
+    // tile = map.getMapOfTiles()[0][0];
+    // tile.setVisited();
+    // tile.setVisited();
+    // tile = map.getMapOfTiles()[0][1];
+  
 
+
+    screenTest.displayMapScreen(map,player);
     return 0;
 }

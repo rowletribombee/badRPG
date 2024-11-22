11ef3a319f78e5de688460fb2a60b46f0303adbf
Sdrabla <sdrab001@ucr.edu>
2024-11-21 17:51:19 -0800

    Screen.cpp cleanup and make tests added

--- test/testScreens.cpp
+++ test/testScreens.cpp
@@ -3,59 +3,55 @@
 #include "../lib/Player.h"
 #include "../lib/tiles/Tile.h"
 #include <iostream>
+#include <sstream>
 using namespace std;
 
-//used g++ -I.testScreens.cpp ../src/Screen.cpp -o screenTest to compile 
 int main()
 {
     Screen screenTest;
 
     //test start screen 
-    // cout << "Start screen:" << endl;
-    // screenTest.displayStartScreen();
+    cout << "Start screen:" << endl;
+    screenTest.displayStartScreen();
 
-    // cout << endl << endl;
+    cout << endl << endl;
 
-    // // //test instruciton screen 
-    // cout << "Instructions: " << endl;
-    // screenTest.displayInstructions();
+    //test instruciton screen 
+    cout << "Instructions: " << endl;
+    screenTest.displayInstructions();
 
-    // //test intro display
-    // cout << "Intro:" << endl;
-    // screenTest.displayIntroScreen();
+    //test intro display
+    cout << "Intro:" << endl;
+    screenTest.displayIntroScreen();
 
-    // test command manu 
-    // cout << "Command screen:" << endl;
-    // screenTest.displayCommandMenu();
+    //test command manu 
+    cout << "Command screen:" << endl;
+    screenTest.displayCommandMenu();
 
-    // //test inventory print 
-    // //Needs items and weapons 
-    // cout << "Inventory screen:" << endl;
-    // screenTest.displayInventory();
+    //test inventory print 
+    //Needs items and weapons 
+    cout << "Inventory screen:" << endl;
+    screenTest.displayInventory();
 
-    // //test credit print
-    // cout << "Credits:" << endl;
-    // screenTest.displayCredits();
+    //test credit print
+    cout << "Credits:" << endl;
+    screenTest.displayCredits();
 
     //test map display
     cout << "Map Display:" << endl;
     
     //create objects
-    Map map;
-    map.initialize();
-
     Player player;
+    Map map;
 
+    istringstream input("w\nd\nd\n");
+    cin.rdbuf(input.rdbuf());
 
-    //mark first tile as visited
-    Tile tile;
-    // tile = map.getMapOfTiles()[0][0];
-    // tile.setVisited();
-    // tile.setVisited();
-    // tile = map.getMapOfTiles()[0][1];
-  
+    player.move(map); // Reads 'w'
+    player.move(map); // Reads 'd'
+    player.move(map); // Reads 'd'
 
-
+    //output map with player currently on (x) [2,1] and [0,0][0,1][1,1] visited (x)
     screenTest.displayMapScreen(map,player);
     return 0;
 }

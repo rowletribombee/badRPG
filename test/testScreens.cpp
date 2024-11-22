8288a85927fe227959a5aa59fc6c6a050f617cff
stanleylew5 <stanley43595@gmail.com>
2024-11-21 16:30:11 -0800

    uncomment code

--- test/testScreens.cpp
+++ test/testScreens.cpp
@@ -5,37 +5,36 @@
 #include <iostream>
 using namespace std;
 
-//used g++ -I.testScreens.cpp ../src/Screen.cpp -o screenTest to compile 
 int main()
 {
     Screen screenTest;
 
     //test start screen 
-    // cout << "Start screen:" << endl;
-    // screenTest.displayStartScreen();
+    cout << "Start screen:" << endl;
+    screenTest.displayStartScreen();
 
-    // cout << endl << endl;
+    cout << endl << endl;
 
-    // // //test instruciton screen 
-    // cout << "Instructions: " << endl;
-    // screenTest.displayInstructions();
+    // test instruciton screen 
+    cout << "Instructions: " << endl;
+    screenTest.displayInstructions();
 
-    // //test intro display
-    // cout << "Intro:" << endl;
-    // screenTest.displayIntroScreen();
+    // test intro display
+    cout << "Intro:" << endl;
+    screenTest.displayIntroScreen();
 
     // test command manu 
-    // cout << "Command screen:" << endl;
-    // screenTest.displayCommandMenu();
+    cout << "Command screen:" << endl;
+    screenTest.displayCommandMenu();
 
-    // //test inventory print 
-    // //Needs items and weapons 
-    // cout << "Inventory screen:" << endl;
-    // screenTest.displayInventory();
+    // test inventory print 
+    // needs items and weapons 
+    cout << "Inventory screen:" << endl;
+    screenTest.displayInventory();
 
-    // //test credit print
-    // cout << "Credits:" << endl;
-    // screenTest.displayCredits();
+    // test credit print
+    cout << "Credits:" << endl;
+    screenTest.displayCredits();
 
     //test map display
     cout << "Map Display:" << endl;
@@ -46,16 +45,12 @@ int main()
 
     Player player;
 
-
     //mark first tile as visited
     Tile tile;
-    // tile = map.getMapOfTiles()[0][0];
-    // tile.setVisited();
-    // tile.setVisited();
-    // tile = map.getMapOfTiles()[0][1];
+    tile = map.getMapOfTiles()[0][0];
+    tile.setVisited();
+    tile = map.getMapOfTiles()[0][1];
   
-
-
     screenTest.displayMapScreen(map,player);
     return 0;
 }

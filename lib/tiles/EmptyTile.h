d54aeba0658d0a811f035fc3aba1db4ce6aa3a80
stanleylew5 <stanley43595@gmail.com>
2024-12-06 13:46:31 -0800

    remove unnecessary func

--- lib/tiles/EmptyTile.h
+++ lib/tiles/EmptyTile.h
@@ -6,7 +6,6 @@ class EmptyTile : public Tile{
     public:
         EmptyTile();
         void print() override;
-        vector<string> getDialogue(){ return dialogue; }
     private:
         vector<string> dialogue = {
             "Ah yes! Exactly what I needed! A whole lot of nothing. \n \n",
